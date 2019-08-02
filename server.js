b8aab773d64610bfd7c54ae1c75db109c71fbdee
CasperTheGhost <32527786+CasperrDeveloper@users.noreply.github.com>
2019-08-02 18:18:11 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ client.on('message', message => {
   const command = args.shift().toLowerCase();
   if (command==='udp'){
   	if (message.member.roles.some(role => role.name === 'booter')) {
-      console.log(`attack! ${args[0]}`);
+      console.log(args[0]);
   	 //message.channel.send(`Attack started on port ${args[1]}.`);
   	    shell.exec(`./hit ${args[0]} ${parseInt(args[1])}`);
   	   // message.reply("Attack finished.");
      }
   }
 });
-client.login('NjA2OTAyNDU0MzY1NDU0MzM2.XURz-g.3yKRoycxyTmlhmuC6v7TgloKiXk');
+client.login('NjA2OTAyNDU0MzY1NDU0MzM2.XUS1Bw.ChEmrsd4s9GEh8vMWPpSC8O9ZRI');
